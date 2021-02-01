9d80841efc8950f2906b9bc5e4ce21349c11ea0e
Time-deep-sea <2830985809@qq.com>
2021-02-01 21:29:59 +0800

    2.1

--- 7-整数反转.js
+++ 7-整数反转.js
@@ -2,8 +2,8 @@ function Reversal(X){
     var str=X.toString()
     var arr=str.split("")
     if(arr[0]!=='-'){
-        var num=Number(arr.reverse().join(''))
-        if(num <= 2147483648 && num >= -2147483648){
+        if(X <= Math.pow(2,31)-1){
+            var num=Number(arr.reverse().join(''))
             return num;
         }else{
             return 0
@@ -11,8 +11,8 @@ function Reversal(X){
     }
     if(arr[0]==="-"){
         delete arr[0]; // 删除负号
-        let num =Number(arr.reverse().join(''));
-        if(num < 2147483648 && num > -2147483648){
+        if(X < Math.pow(2,31)-1){
+            let num =Number(arr.reverse().join(''));
             return ~num+1;
             // ~ 取反运算符,取反的时候会自动减1,所以取反在加上
         }else{
@@ -20,4 +20,4 @@ function Reversal(X){
         }
     }
 }
-console.log(Reversal(214748368))
+console.log(Reversal(Math.pow(2,30))*-1)
